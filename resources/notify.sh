365d767c0405e31515f9a143c3bfe0fcb57058f2
Antonio M M <antoniomm80@gmail.com>
2024-02-03 20:44:03 +0100

    feat: Añade Telegram como proveedor de notificaciones

--- resources/notify.sh
+++ resources/notify.sh
@@ -1,3 +1,8 @@
 #/bin/sh
-
-curl -X POST -d "message=$1" $NOTIFICATION_WEBHOOK_URL
+if [ "$NOTIFICATION_PROVIDER" = "Telegram" ]
+    then
+        curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_API_KEY/sendMessage" \
+             -d "chat_id=$TELEGRAM_CHAT_ID" \
+         -d "text=$1"
+    fi
+#curl -X POST -d "message=$1" $NOTIFICATION_WEBHOOK_URL
